823d201883a91dfef7d56bcdd82aab17f127e196
kellyngton <kelldias895@gmail.com>
2022-09-05 13:33:30 -0300

    Project created

---
+++ Backend/src/main/java/com/kellyngton/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.kellyngton.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsmetaApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/kellyngton/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kellyngton.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
